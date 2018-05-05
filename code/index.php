eceaa9b3e5fbfe37231a73cbfa2c22b64274c385
Jason Chantry <jason.chantry@gmail.com>
2018-05-05 14:37:12 -0600

    craeted code folder
    This is where all of my code will be saved

---
+++ code/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     	<meta charset="utf-8"> 
+	    <meta name="author" content="Jason Chantry">
+	    <meta name="description" content="JasonsWebsite">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> CIT261:02 - JChantry's Site </title>
+        <link rel="stylesheet" href="css/main.css">  
+        <script                     src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    </head>
+    <header class="header">
+        <img  src="images/byui2.jpg" alt="Logo" >
+        <h1 >Jason Chantry</h1>
+        <h2>CIT261:02 Mobile Application Development</h2>
+        
+    </header>
+    <nav class="page-nav">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/cit261/modules/menu.php'; ?>
+    </nav>
+    <main>
+        <h3>Welcome to my CIT261 website. On this page I will demonstrate fluency in each of the 13 topics required in the course. These topics are as follows:</h3>
+        <h4>Coding Topics</h4>
+        <p>1. JavaScript – Loops, Conditional Statements, Functions, Arrays</p>
+        <p>2. JavaScript – Object Creation Functions, Properties, Methods, Instantiation</p>
+        <p>3. JSON Parse, Stringify</p>
+        <p>4. Using AJAX to Consume a JSON Web Service**</p>
+        <p>   **Pay special attention to this one. The rubric and other parts of the course are misleading and most people think they just need to use AJAX to read the content of a file. That is NOT the requirement. The requirement is for you to call a 3rd party Web Service (aka API). This is the most complex of the topics, so start reading up on it if you don't know much about it. I'll will send out more info about it in a few weeks.</p>
+        <p>5. Local Storage Storing and Retrieving Arrays, Associative Arrays, and Objects</p>
+        <p>6. DOM Manipulation Using createElement, appendChild, insertBefore etc.</p>
+        <p>7. Manipulating CSS Class Properties Using JavaScript</p>
+        <p>8. CSS3 Transitions and Animations and triggering them with JavaScript</p>
+        <p>9. JavaScript Events Especially for Mobile Devices (onTouchBegin) and Animation and Transition Events</p>
+        <p>10. HTML5 Tags - Video, Audio, and Canvas</p>
+        <p>11. Designing, Defining, and Triggering CSS3 Transitions without Custom Libraries</p>
+        <p>12. Designing, Defining, and Triggering CSS3 Transforms without Custom Libraries</p>
+        <p>13. Designing, Defining, and Triggering CSS3 Animations without Custom Libraries</p>
+        
+        
+  
+        
+        <h4>Group Schedule</h4>
+        <p> <a href="https://docs.google.com/spreadsheets/d/1tRzWE3YmMH7698RniNFN43vsIDxAkMrpduzHHpecsUw/edit#gid=0">CIT261 - Group 6 Schedule (GSheet)</a></p>
+        <p>Group 6 will meet Wednesdays at 7:30pm MST via Google Hangouts</p>
+
+    </main>
+    <footer>
+    	<p class="obj_Ctr"> CIT 261:02 I <a href=http://jasonchantry.pw/cit261>jasonchantry.pw/cit261</a> I &copy; 2018 Jason Chantry. All Rights Reserved. </p>
+    </footer>
+</html>
